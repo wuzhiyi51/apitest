21786fecdac4e6fab09f8941a2f2482e2982ab86
WuZhiYi <wuzhiyi51@gmail.com>
2019-07-19 11:14:56 +0800

    return json

--- web/views.py
+++ web/views.py
@@ -1,12 +1,24 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render_to_response
 from django.shortcuts import render
+import json
 
 # Create your views here.
 
 def Login(request):
     if request.method=='POST':
+        result = {}
         username = request.POST.get('username')
-        return HttpResponse(username)
+        password = request.POST.get('password')
+        result['username']=username
+        result['password']=password
+        data=json.dumps(result)
+        return HttpResponse(data, content_type='application/json;charset=utf-8')
+
+    # if request.method=='GET':
+    #     username=request.GET.get('username')
+    #     mobile=request.GET.get('mobile')
+    #     return HttpResponse(username + mobile)
+
     else:
         return render_to_response('login.html')
